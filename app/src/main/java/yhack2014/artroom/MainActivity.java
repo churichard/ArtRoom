434c747e62b2e9103d25281378bd4a50bed3d483
Daven Wu <epikglow@gmail.com>
2014-11-01 10:04:08 -0400

    Moxtra doesn't work

--- app/src/main/java/yhack2014/artroom/MainActivity.java
+++ app/src/main/java/yhack2014/artroom/MainActivity.java
@@ -4,16 +4,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.moxtra.sdk.MXAccountManager;
+import com.moxtra.sdk.MXSDKConfig;
+import com.moxtra.sdk.MXSDKException;
+
+import java.util.UUID;
 
 public class MainActivity extends Activity {
+    MXAccountManager acctMgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        try {
+            acctMgr = MXAccountManager.createInstance(this, "u-DTusZ0ruc", "vWLjlkGMZOk");
+        } catch (MXSDKException.InvalidParameter invalidParameter) {
+            invalidParameter.printStackTrace();
+        }
+
+        MXSDKConfig.MXUserInfo userInfo = new MXSDKConfig.MXUserInfo(UUID.randomUUID().toString(), MXSDKConfig.MXUserIdentityType.IdentityUniqueId);
+        MXSDKConfig.MXProfileInfo profile = new MXSDKConfig.MXProfileInfo("Daven", "Wu", null);
+        acctMgr.setupUser(userInfo, profile, null, new MXAccountManager.MXAccountLinkListener(){
+            @Override
+            public void onLinkAccountDone(boolean bSuccess){
+                if(bSuccess) {
+                    Toast.makeText(getBaseContext(), "Linked account!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getBaseContext(), "Account link failed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
