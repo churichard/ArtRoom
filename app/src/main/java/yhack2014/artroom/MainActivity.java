a4d9e9759bc55a09de5c79ad8e2fc192860bc05d
Richard Chu <richardchu10@gmail.com>
2014-11-01 04:41:06 -0400

    Added Estimote functionality
    Outputs the ID of the closest Estimote beacon

--- app/src/main/java/yhack2014/artroom/MainActivity.java
+++ app/src/main/java/yhack2014/artroom/MainActivity.java
@@ -2,18 +2,83 @@ package yhack2014.artroom;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.Utils;
+
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    private BeaconManager beaconManager;
+    private static final String ESTIMOTE_PROXIMITY_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+    private static final Region ALL_ESTIMOTE_BEACONS = new Region("regionId", ESTIMOTE_PROXIMITY_UUID, null, null);
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beaconManager = new BeaconManager(this);
+        textView = (TextView) findViewById(R.id.textView);
+
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
+                double minDistance = Integer.MAX_VALUE;
+                Beacon closestBeacon = null;
+                for (int i = 0; i < beacons.size(); i++) {
+                    double dist = Utils.computeAccuracy(beacons.get(i));
+                    if (dist < minDistance) {
+                        minDistance = dist;
+                        closestBeacon = beacons.get(i);
+                    }
+                }
+
+                textView.setText("Minor value of the closest beacon: " + closestBeacon.getMinor());
+                Log.d(TAG, "Number of beacons: " + beacons.size());
+            }
+        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                try {
+                    beaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
+                } catch (RemoteException e) {
+                    Log.e(TAG, "Cannot start ranging", e);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        try {
+            beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Cannot stop but it does not matter now", e);
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        beaconManager.disconnect();
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
